e2b59a62f237a971d971659e2c9ba3e4c0164f9b
matiasmartin <matiass.lean@gmail.com>
2025-04-20 14:19:10 +0200

    feat: add RegisterForReflection for native build

--- src/main/java/com/mmartin/authms/infrastructure/presentation/dto/ErrorResponse.java
+++ src/main/java/com/mmartin/authms/infrastructure/presentation/dto/ErrorResponse.java
@@ -1,8 +1,10 @@
 package com.mmartin.authms.infrastructure.presentation.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Builder;
 
 @Builder
+@RegisterForReflection
 public record ErrorResponse (
         Integer status,
         String code,
